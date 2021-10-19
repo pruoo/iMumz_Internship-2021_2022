ee2e30b432844a9032667ee615b564a049ccd9fe
ishan_jawzz <ishanacharyya6@gmail.com>
2021-10-19 22:13:35 +0530

    Final

---
+++ ishan_acharyya/coding_task/design/codingtask/android/app/src/main/kotlin/com/example/codingtask/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codingtask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ishan_acharyya/coding_task/design/codingtask/lib/constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color whiteBackground=Colors.white;
+const Color greenAccent  =Color(0xffc1ebe1);
+const Color orangeText = Color(0xfff49a60);
+const Color blueReferralNumbering = Color(0xff65c6e9);
+
+const double paddingHorizontal=20;
+void doSomething(){
+  print("Something was interacted with");
+}
