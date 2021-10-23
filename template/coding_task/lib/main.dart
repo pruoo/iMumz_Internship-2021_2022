8b23457fd7df7bb805f41c63e585d1caa6f50048
Shubham <vivekgoyal164@gmail.com>
2021-10-23 15:35:33 +0530

    Init

---
+++ template/coding_task/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:coding_task/main_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'iMumz_Internship-2021_2022',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
